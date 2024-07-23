e630082d1944815e2b65f83dac366af22b4e356b
ez23re <ez23re@gmail.com>
2024-07-24 00:35:12 +0900

    컨트롤러 검증 로직 인터셉터로 분리

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -20,30 +20,19 @@ public class WishController {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Page<Wish>> getWishes(
-            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token,
-            Pageable pageable) {
-        if (token == null || !token.startsWith("Bearer ")) {
-            return ResponseEntity.status(401).build();
-        }
+    public ResponseEntity<Page<Wish>> getWishes(Pageable pageable) {
         Page<Wish> wishes = wishService.getWishes(pageable);
         return ResponseEntity.ok(wishes);
     }
 
     @PostMapping("/add")
     public ResponseEntity<Wish> addWish(@RequestBody WishRequest wishRequest, @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token) {
-        if (token == null || !token.startsWith("Bearer ")) {
-            return ResponseEntity.status(401).build();
-        }
         Wish wish = wishService.addWish(token, wishRequest);
         return ResponseEntity.ok(wish);
     }
 
     @DeleteMapping("/remove")
     public ResponseEntity<Void> removeWish(@RequestBody WishRequest wishRequest, @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token) {
-        if (token == null || !token.startsWith("Bearer ")) {
-            return ResponseEntity.status(401).build();
-        }
         wishService.removeWish(token, wishRequest.getProductId());
         return ResponseEntity.ok().build();
     }
